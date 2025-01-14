e5ebb7b8bfc60d728b2636414bffa60919e3ff74
tyk22 <tykkor22@gmail.com>
2025-01-14 16:52:54 +0900

    [kty] 아무것도 안함

--- mini_project_cinema/src/com/movie/view/Menu.java
+++ mini_project_cinema/src/com/movie/view/Menu.java
@@ -1,5 +1,14 @@
 package com.movie.view;
 
-public class Menu {
+import java.util.List;
 
+import com.movie.controller.ScreeningController;
+import com.movie.model.vo.Screening;
+
+public class Menu {
+	private ScreeningController screening = new ScreeningController();
+	public void movieRank() {
+		System.out.println("=-=인기 순위=-= ");
+//		List<Screening> list = screening.movieRank();
+	}
 }
