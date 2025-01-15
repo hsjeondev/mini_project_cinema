971cf3d978f1aba4e9b73b709db559c25fca598c
hsjeondev <hsjeondev@gmail.com>
2025-01-15 16:45:19 +0900

    [jhs] 본인 예매 내역 확인 작업 중

--- mini_project_cinema/src/com/movie/model/dao/ReservationDao.java
+++ mini_project_cinema/src/com/movie/model/dao/ReservationDao.java
@@ -1,15 +1,23 @@
 package com.movie.model.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.movie.model.vo.Reservation;
 
 public class ReservationDao {
 
 	public List<Reservation> checkReservation(int userNo, Connection conn) {
-		List<Reservation> list = new ArrayList();
+		List<Reservation> list = new ArrayList<Reservation>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		
 		String sql = "SELECT m.movie_title AS title, t.theater_no AS theaterNo, s.screening_date AS date, s.screening_time AS time "
 				+ " FROM reservation r "
@@ -22,6 +30,28 @@ public class ReservationDao {
 				+ " JOIN theater t "
 				+ " ON s.theater_no = t.theater_no "
 				+ " WHERE u.user_no = ? ";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, userNo);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Map<String, Object> map = new HashMap<String, Object>();
+				String title = rs.getString("title");
+				int theaterNo = rs.getInt("theaterNo");
+				DateTimeFormatter dateDtf = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+				DateTimeFormatter timeDtf = DateTimeFormatter.ofPattern("a hh시 mm분");
+				String date = rs.getTimestamp("date").toLocalDateTime().format(dateDtf);
+				String time = rs.getTimestamp("time").toLocalDateTime().format(timeDtf);
+				map.put("title", title);
+				map.put("theaterNo", theaterNo);
+				map.put("date", date);
+				map.put("time", time);
+				
+			}
+		} catch(SQLException e) {
+			list = null;
+		}
 		
 		return list;
 	}
